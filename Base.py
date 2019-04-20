759168a67f2ca5e9bd642da53aee44d5c977a027
alex <benjamin.win@foxmail.com>
2019-04-20 21:44:29 +0800

    第三版，差不多大致的功能已经完成了，就差明天的整合了，不过还有一个行列的问题，没解决，东西在Maya和goolg浏览器里可以找到。

--- Base.py
+++ Base.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
+import os
+import shutil
 from maya import cmds
 import pymel.core as pm
 
@@ -9,27 +11,27 @@ sgNodesList = cmds.ls(type='shadingEngine')
 if 'initialParticleSE' in sgNodesList or 'initialShadingGroup' in sgNodesList:
     sgNodesList.remove('initialParticleSE')
     sgNodesList.remove('initialShadingGroup')
-
     # <2>.通过材质的SG节点找到对应物体或者是面级别
 row = 0
 for index, sgNode in enumerate(sgNodesList):
+    colorChannel = 'color'
     coulmn = (index) / 10
+    print "row+1+coulmn*10", row + 1 + coulmn * 10 + 1000
     if row == 10:
         row = 0
     print "%s-->%s-->%s" % (row, coulmn, sgNode)
-    # row += 1
+    row += 1
     cmds.hyperShade(objects=sgNode)  # 返回值是直接选择物体，并非节点
 
     # <3>.通过SG节点重命名找到材质球所连接的贴图文件并保存
     materialNode = cmds.listConnections(sgNode + '.surfaceShader')
     print "materialNode:", materialNode
 
-    fileNode = cmds.listConnections('%s.color' % (materialNode[0]), type='file')
+    fileNode = cmds.listConnections('%s.%s' % (materialNode[0], colorChannel), type='file')
     currentFile = cmds.getAttr("%s.fileTextureName" % fileNode[0])
     print "currentFile:", currentFile
 
     # 2.对于相同材质球的obj，进行UV同一象限排布，没有重叠
-
     # <1>.通过‘被选择的’物体找到UV点
     selUVs = pm.polyListComponentConversion(tuv=1)
     print 'selUVs:', selUVs
@@ -42,11 +44,29 @@ for index, sgNode in enumerate(sgNodesList):
 
     # <3>.通过UV点的位置象限重命名该材质球所连接的贴图文件,命名遵循 <channel>.#.jpg
     # (1).链接函数处理贴图文件
-    dealTexture(currentFile, coulmn, row)
+    if row + 1 == 10:
+        name = str(coulmn + 1) + '0'
+    else:
+        # print "dealTexture-->%s--->%s" %(coulmn,row+1)
+        name = str(coulmn) + str(row + 1)
+    channelName = colorChannel + '.10' + name
+    print "%s----->" % channelName
+    newPath = dealTexture(currentFile, channelName)
+    # cmds.setAttr(materialNode[0]+'_AlbedoMapFile.fileTextureName',newPath,type = "string")
+    cmds.setAttr("%s.fileTextureName" % fileNode[0], newPath, type="string")
+
     row += 1
 
 
 # 4.处理贴图文件函数
-def dealTexture(texturePath, coulmn, row):
-    print "dealTexture-->%s--->%s" % (coulmn, row)
+def dealTexture(filePath, channelName):
+    print "filePath:", filePath
+    # print "channelName:",channelName
+    fileName = os.path.basename(filePath).split('.tif')[0]
+    print "fileName:", fileName
+    texturePath = filePath.replace(fileName, channelName)
+    print "finnaly path:", texturePath
+    # os.rename(filePath,texturePath)
+    shutil.copy(filePath, texturePath)
+    return texturePath
 
