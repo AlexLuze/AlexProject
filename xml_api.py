0119abf53ab0fb2e7c4e00067c6de72f5c0561e9
alex <benjamin.win@foxmail.com>
2019-04-28 11:01:56 +0800

    xml api 完整版。

--- xml_api.py
+++ xml_api.py
@@ -1,33 +1,44 @@
-import sys
-
-sys.path.append(r'C:/Users/benja/Documents/maya/2018/scripts/base_test_2/')
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+This module is an API for writing and reading xml file.
+Here are the functions in the module:
+    xml_write(): Writing the dictionary information obtained from the material_api file to the XML file.
+    xml_read(): Read the XML file information,
+                save as the corresponding parameter,
+                and to create a material sphere to the corresponding geometries.
+"""
 from xml.dom.minidom import Document
 from xml.dom.minidom import parse
-import xml.dom.minidom
-import material_lookdev_api as mat_dev_api
+import material_api as mat_dev_api
 
 
-# xml_file_path = "C:/Users/benja/Documents/maya/2018/scripts/base_test_2/base003.xml"
 def xml_write(xml_file_path):
-    relationship_infomations_list = mat_dev_api.get_infomation_for_relationship()
-    # pprint.pprint(relationship_infomations_list)
+    """
+    Writing the dictionary information obtained from the material_lookdev_api file to the XML file.
+    :param str xml_file_path: The absolute path to the xml.
+    :return: True
+    :rtype: bool
+    """
+
+    # Get the material sphere information of the current scene in Maya and return it in dictionary format.
+    relationship_information_list = mat_dev_api.get_information_for_relationship()
+
     root = Document()
-    f = open(xml_file_path, 'w')
+    xml_file = open(xml_file_path, 'w')
 
     relationship = root.createElement('relationship')
     root.appendChild(relationship)
-    # print len(relationship_infomations_list['material'])
-    # print type(relationship_infomations_list['material'])
-    for info_dic in relationship_infomations_list['material']:
-        # print "info_dic:",info_dic
+    for information in relationship_information_list['material']:
 
-        material_name = info_dic['material_name']
-        material_type = info_dic['material_type']
-        # print "material_type:",material_type
-        sg_node = info_dic['sg_node_name']
-        obj_info_list = info_dic['objects']
-        tex_info = info_dic['texture']
+        # Read the information in the dictionary and save it.
+        material_name = information['material_name']
+        material_type = information['material_type']
+        sg_node = information['sg_node_name']
+        obj_info_list = information['objects']
+        tex_info = information['texture']
 
+        # Create the corresponding element information in the XML file.
         material_xml = root.createElement('material')
         material_xml.setAttribute('material_name', material_name)
         relationship.appendChild(material_xml)
@@ -47,49 +58,48 @@ def xml_write(xml_file_path):
         for obj_info in obj_info_list:
             obj = root.createElement("objects")
             material_xml.appendChild(obj)
-            # obj.appendChild(root.createTextNode(obj_info))
             obj.setAttribute('object', obj_info)
-        # break
-    f.write(root.toprettyxml(indent="    "))
-    f.close()
+
+    xml_file.write(root.toprettyxml(indent="    "))
+    xml_file.close()
 
     return True
 
 
-# r'C:\Users\benja\Documents\maya\2018\scripts\base_test_2/base002.xml'
-def read_xml(xml_file_path):
-    objects_list = []
-
-    dom = xml.dom.minidom.parse(xml_file_path)
-
+def xml_read(xml_file_path):
+    """
+    Read the XML file information,
+    save as the corresponding parameter,
+    and to create a material sphere to the corresponding geometries.
+    :param str xml_file_path: The absolute path of the XML file to be read.
+    :return: True
+    :rtype: bool
+    """
+    dom = parse(xml_file_path)
     root = dom.documentElement
-
     materials = root.getElementsByTagName("material")
-    print "materials:", materials
 
     for material in materials:
+
+        objects_list = []
+
         mat_name = material.getAttribute("material_name")
-        print "mat_name:", mat_name
 
         mat_type = material.getElementsByTagName('material_type')[0]
         material_type = mat_type.childNodes[0].data
-        print "material_type:", material_type
 
         sg_node = material.getElementsByTagName('sg_node_name')[0]
         sg_node_name = sg_node.childNodes[0].data
-        print "sg_node_name:", sg_node_name
 
         texture_file = material.getElementsByTagName('texture_file')[0]
         file_path = texture_file.childNodes[0].data
-        print "file_path:", file_path
 
         objects_xml_list = material.getElementsByTagName('objects')
 
         for obj in objects_xml_list:
             object_poly = obj.getAttribute('object')
             objects_list.append(object_poly)
-        print "objects_list:", objects_list
 
-        mat_dev_api.create_material_node(mat_name, material_type, sg_node_name, file_path, corresponding_model)
-
-# ren = read_xml(xml_file_path)
+        # Create a texture sphere and apply it to the geometries.
+        mat_dev_api.create_material_node(mat_name, material_type, sg_node_name, file_path, objects_list)
+    return True
